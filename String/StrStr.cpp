39acddca33d142a529b9260ac633ba946fda9d13
freesouls <declanxu@gmail.com>
2015-08-15 20:47:41 +0800

    8.15

---
+++ String/StrStr.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    /**
+     * Returns a index to the first occurrence of target in source,
+     * or -1  if target is not part of source.
+     * @param source string to be scanned.
+     * @param target string containing the sequence of characters to match.
+     */
+    int strStr(const char *source, const char *target) {
+        // write your code here
+        if (target == NULL || source == NULL) {
+            return -1;
+        }
+
+        int sLen = strlen(source);
+        int tLen = strlen(target);
+
+        int i = 0;
+        int j = 0;
+        while (i < sLen && j < tLen ) {
+            if (source[i] == target[j]){
+                i++;
+                j++;
+            }
+            else{
+                i = i - j + 1;
+                j = 0;
+            }
+        }
+
+        if (j == tLen) {
+            return i - j;
+        }
+
+        return -1;
+    }
+};
