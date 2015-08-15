1e1f14862aabf43605b436717cae70b78b50ee98
freesouls <declanxu@gmail.com>
2015-08-15 20:59:47 +0800

    8.15

--- String/StrStr.cpp
+++ String/StrStr.cpp
@@ -12,6 +12,32 @@ public:
             return -1;
         }
 
+        int i = 0;
+        int j = 0;
+        while (*(source+i) != '\0' && *(target+j) != '\0' ) {
+            if (source[i] == target[j]){
+                i++;
+                j++;
+            }
+            else{
+                i = i - j + 1;
+                j = 0;
+            }
+        }
+        // it is the same as *(target+j) == 0
+        if (*(target+j)=='\0') {
+            return i - j;
+        }
+
+        return -1;
+    }
+
+    int strStr1(const char *source, const char *target) {
+        // write your code here
+        if (target == NULL || source == NULL) {
+            return -1;
+        }
+
         int sLen = strlen(source);
         int tLen = strlen(target);
 
